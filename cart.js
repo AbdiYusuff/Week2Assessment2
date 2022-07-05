2aad5dd968ff6b4473d9dcc944a296d2a352c608
Abdi Yusuf <ayusufadan@gmail.com>
2022-07-05 15:29:10 -0500

    Week2Assessment2Finished

---
+++ cart.js
@@ -0,0 +1,111 @@
+///////////////////////////////////////////////
+///////////////////CART.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code to
+    calculate order totals. You'll also be 
+    creating customer objects.  
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Below is a cart array that has food objects
+    inside. 
+
+    Write a callback below that uses the reduce
+    array method to calculate the sum of all
+    the food. 
+*/
+
+const cart = [
+    {
+        name: 'pizza', 
+        price: 9.99
+    }, 
+    {
+        name: 'pasta', 
+        price: 8.99
+    }, 
+    {
+        name: 'salad', 
+        price: 7.99
+    }
+]
+
+//CODE HERE
+
+const summedPrice = cart.reduce((a, c) => (a + c.price), 0)
+//console.log(summedPrice)
+
+
+//////////////////PROBLEM 2////////////////////
+/*  
+    Write a function called `calcFinalPrice` that
+    can take in `cartTotal`,`couponValue`,
+    and `tax` arguments. 
+
+    Inside the function, calculate the tax 
+    on the cartTotal and add it in. Subtract
+    the value of the coupon. Return the final
+    number. 
+
+    Note: the numbers passed in for `tax` will be
+    decimals, for example: .06 for a 6% tax.
+*/
+
+//CODE HERE
+
+const calcFinalPrice = (cartTotal, couponValue, tax) => {
+    const applyTaxCoupon = cart.reduce((a, c) => a + c.price*(1-tax))
+    return (applyTaxCoupon - couponValue)
+}
+
+calcFinalPrice(cart, 1, 0.6)
+//console.log(cart)
+
+//////////////////PROBLEM 3////////////////////
+/*  
+    In this problem, you'll create a model for 
+    a customer object as well as an example
+    object. 
+
+    Plan out a customer object for the cart page.
+    Think about the information that a 
+    restaurant would need about its customers.
+
+    In the TEXT ANSWER area below, describe the
+    properties that your customer object will have
+    and why you chose those properties.
+
+    Explain what data types each property should be
+    and why you chose those data types. 
+
+    Your object should have at least 4 properties. 
+*/
+
+/*
+    TEXT ANSWER HERE
+
+*/ 
+// The restaurant would need: 
+// 1. Name of the customer. The restaurant needs to know who's ordering the food. Should be a String. 
+// 2. Phone Number. The restaurant needs to know contact number to get notified. Should be a string datatype. Could also be a number 
+// 3. Any food allergies. The restaurant needs to know if the customer has allergies. Should be a string
+// 4. Zipcode.The restaurant needs to know the zicode to check the delivery availability. Shoud be a number datatype. 
+// 5. Whether or Not the customer would like to receive text from the restaurant to confirm their orders or update them on their deliveries 
+
+/*
+    Now, create a customer object following your own
+    guidelines.
+*/
+
+//CODE HERE
+
+const customer =[{
+    name : "Abdi"},
+    {phoneNumber: '222-222-2222'},
+    {Allergies: "Peanuts"},
+    {Zipcode: 55021},
+    {receiveText: ["Yes", "No"]}
+]
---
+++ read.md
@@ -0,0 +1,37 @@
+Link to the Figma Page: https://www.figma.com/file/Z8DQuVmA1tT7HrwCFTqKKW/Somalicious?node-id=0%3A1
+
+
+Extra Credit Research Assignment: 
+
+Identify 3 software or technology companies the interest you.
+Conduct research to figure out some or all of the elements of each company’s technology stack. Check job openings/job descriptions, engineering blogs, and anything else that may have information about what the company uses to write/build it’s software products.
+
+Record your findings in a google doc. If you can find information about why the company chose that stack, include that as well.
+
+Company 1: Google
+Technology Stack includes Python, Java, Android SDK, C++, 
+Preact, LevelDB
+
+Careers: 
+A common job description is a "Bachelor's degree or equivalent practical experience" though what counts as an equivalent experience has not be disclosed. As a bootcamp graduate, I might be able to gain relevant experience but I'm still wondering how long of an experience is sufficient
+Software Engineering job descriptions include experience in data structures and algorithms, which are topics we will barely touch on in our program. 
+
+
+Company 2: Amazon
+
+Tech Stack: 
+Java, Perl, Angular JS, MySQL, Amazon EC2 container service, DynamoDB
+
+Product Development Strategy: 
+Accroding to Codeinspiration, Amazon uses a product development method called "Working Backwards". Product Management teams roll out a press-release or a simple, short, and engaging description of the product as they envision from the customers perspective. They iteterate on the press release until the managers reach an agreement that this idea is ready to proceed to prototyping
+
+
+Company 3: Facebook
+Tech Stach: PHP, React, GraphQL, Memcached, Cassandra, Hadoop, MySQL, Swift, C++, PHP, JavaScript, JSON, HTML, CSS.
+
+Software Engineer Qualifications:
+Programming Languages include JavaScript, PHP, HTML, CSS, XHTML, C, C++, C#, or Java
+Knowledge in algorithms, object-oriented programming, data structures, relational DB,operating Systems,  
+Software Development Tools: Code editors (VIM) and revision control systems (GIT) 
+
+
